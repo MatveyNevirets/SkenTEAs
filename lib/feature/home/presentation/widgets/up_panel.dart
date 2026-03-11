d8a4b0d1ebeea4202029c1ef5c8384e09e1fc9fb
ErOPKO3AOB <92738166+ErOPKO3AOB@users.noreply.github.com>
2026-03-11 12:10:28 +0300

    Up panel in homescreen changed

--- lib/feature/home/presentation/widgets/up_panel.dart
+++ lib/feature/home/presentation/widgets/up_panel.dart
@@ -1,21 +1,23 @@
 import "package:flutter/material.dart";
+import "package:flutter_svg/svg.dart";
 
 class UpPanel extends StatelessWidget {
   const UpPanel({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final colorTheme = Theme.of(context);
     final colorScheme = Theme.of(context).colorScheme;
 
     return Row(
       children: [
-        Expanded(
-          child: Icon(
-            Icons.heart_broken_rounded,
-            size: 40,
-            color: colorScheme.onPrimary,
+        SvgPicture.asset(
+          'assets/images/logo.svg',
+            width: 50,
+            height: 50,
+            fit: BoxFit.contain,
           ),
-        ),
+          SizedBox(width: 20),
         Expanded(
           flex: 2,
           child: RichText(
@@ -25,7 +27,7 @@ class UpPanel extends StatelessWidget {
                 TextSpan(
                   text: "TEA",
                   style: TextStyle(
-                    color: Colors.lightGreenAccent,
+                    color: colorTheme.splashColor,
                     fontSize: 20,
                   ),
                 ),
